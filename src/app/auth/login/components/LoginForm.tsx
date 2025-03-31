3f3d9ff860fcca0d30b88e39763f3bc4fb5b0c6d
harmindervats <harminder.singh@vastites.com>
2025-03-31 10:25:09 +0530

    fix buglist point

--- src/app/auth/login/components/LoginForm.tsx
+++ src/app/auth/login/components/LoginForm.tsx
@@ -15,7 +15,7 @@ import { login } from "@/lib/auth";
 
 const FormSchema = z.object({
   username: z.string().min(3, { message: "Username must be at least 3 characters long" }),
-  password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+  password: z.string().min(4, { message: "Password must be at least 4 characters long" }),
 });
 
 type FormSchemaType = z.infer<typeof FormSchema>;
