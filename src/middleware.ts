5d8bbba25efbaa1a5f998999dd09ddfa616db8e3
harmindervats <harminder.singh@vastites.com>
2025-06-12 15:53:52 +0530

    remove middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,16 +5,16 @@ import { roleMiddleware } from "./middleware/roleMiddleware"
 
 export function middleware(req: NextRequest) {
   // authMiddleware - Check if user is logged in
-  const response = authMiddleware(req)
-  if (response) {
-    return response
-  }
+  // const response = authMiddleware(req)
+  // if (response) {
+  //   return response
+  // }
 
-  // roleMiddleware - Check if user has permission for the route
-  const roleResponse = roleMiddleware(req)
-  if (roleResponse) {
-    return roleResponse
-  }
+  // // roleMiddleware - Check if user has permission for the route
+  // const roleResponse = roleMiddleware(req)
+  // if (roleResponse) {
+  //   return roleResponse
+  // }
 
   return NextResponse.next()
 }
